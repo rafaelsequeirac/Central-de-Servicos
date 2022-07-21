d04d6827eec680bacff9927b8804c6a6ad630989
Rafael <rafaelsequeira.c@gmail.com>
2022-07-21 15:01:46 -0300

    Ok 1

---
+++ script.js
@@ -0,0 +1,168 @@
+const list = document.querySelector(".list");
+const grid = document.querySelector(".grid");
+const chamado = document.querySelector(".lista_chamados");
+const btnList = document.querySelector("#list");
+const btnGrid = document.querySelector("#grid");
+const desc = document.querySelector(".desc");
+
+
+        grid.addEventListener("click", () => {
+            chamado.classList.add("grid");
+            btnList.classList.add("on");
+            btnGrid.classList.add("off");
+            desc.classList.add("off");
+        })
+
+        list.addEventListener("click", () => {
+            chamado.classList.remove("grid");
+            btnList.classList.remove("on");
+            btnGrid.classList.remove("off");
+            desc.classList.remove("off");
+        })
+
+//.................Paginação....................//
+
+const chamados = Array.from(document.getElementsByClassName("chamados"));
+
+const html = {
+    get(element) {
+        return document.querySelector(element)
+    }
+}
+
+const state = {
+    page: 1,
+    perPage: 6,
+    totalPage: Math.ceil(chamados.length / 6)
+}
+
+const controls = {
+    next() {
+        state.page++
+
+        const lastPage = state.page > state.totalPage
+        if(lastPage){
+            state.page--
+        }
+    },
+    prev() {
+        state.page--
+
+        if(state.page < 1){
+            state.page++
+        }
+    },
+    goTo(page) {
+        if(page < 1){
+            page = 1
+        }
+
+        state.page = +page
+
+        if(page > state.totalPage){
+            state.page = state.totalPage
+        }
+    },
+    createListeners() {
+        html.get('.first').addEventListener('click', () =>{
+            controls.goTo(1)
+            update()
+        })
+
+        html.get('.last').addEventListener('click', () =>{
+            controls.goTo(state.totalPage)
+            update()
+        })
+
+        html.get('.next').addEventListener('click', () =>{
+            controls.next()
+            update()
+        })
+
+        html.get('.prev').addEventListener('click', () =>{
+            controls.prev()
+            update()
+        })
+    }
+}
+
+const lista = {
+    create(lista_chamados) {
+        const div = document.createElement('div')
+        div.classList.add('chamados')
+        div.innerHTML = 'ok'
+        html.get('.lista_chamados').appendChild(div)
+    },
+    update() {
+        html.get('.lista_chamados').innerHTML = ""
+
+        let page = state.page - 1
+        let start = page * state.perPage
+        let end = start + state.perPage
+        
+        const paginatedItems = chamados.slice(start, end)
+
+        paginatedItems.forEach(lista.create)
+    }
+}
+
+const buttons = {
+    create(number) {
+        const button = document.createElement('div')
+
+        button.innerHTML = number;
+
+        if(state.page == number){
+            button.classList.add('active')
+        }
+
+        button.addEventListener('click', (event) =>{
+            const page = event.target.innerHTML
+
+            controls.goTo(page)
+            update()
+        })
+
+        html.get('#paginate .numbers').appendChild(button)
+    },
+    update() {
+        html.get('#paginate .numbers').innerHTML = ""
+        const {maxLeft, maxRight} = buttons.calculateMaxVisible() 
+
+        for(let page = maxLeft; page <= maxRight; page++){
+            buttons.create(page)
+        }
+    },
+    calculateMaxVisible() {
+        let maxLeft = (state.page - Math.floor(5 / 2))
+        let maxRight = (state.page + Math.floor(5 / 2))
+
+        if(maxLeft < 1) {
+            maxLeft = 1
+            maxRight = 5
+        }
+
+        if(maxRight > state.totalPage) {
+            maxLeft = state.totalPage - (5 - 1)
+            maxRight = state.totalPage
+
+            if(maxLeft < 1) maxLeft = 1
+        }
+
+        return{maxLeft, maxRight}
+    }
+}
+
+function update() {
+    lista.update()
+    buttons.update()
+}
+
+function init() {
+    update()
+    controls.createListeners()
+}
+
+init()
+
+
